f32564d06adfa42ef03929b1ff5075b6adbfff93
leandrocgsi <leandrocgsi@gmail.com>
2017-10-02 22:29:31 -0300

    Hello Android I'm back

---
+++ 01_HelloAndroid/app/src/main/java/br/com/erudio/a01_helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.erudio.a01_helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
