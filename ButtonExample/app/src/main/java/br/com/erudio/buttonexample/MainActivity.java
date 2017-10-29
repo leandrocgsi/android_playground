55c8cb2acdb0080b0d825b8b24046fbc70f10d0b
leandrocgsi <leandrocgsi@gmail.com>
2017-10-29 19:20:33 -0200

    Implementing ButtonExample

--- ButtonExample/app/src/main/java/br/com/erudio/buttonexample/MainActivity.java
+++ ButtonExample/app/src/main/java/br/com/erudio/buttonexample/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked(View view){
+
+        EditText myTextField = (EditText) findViewById(R.id.textField);
         Log.i("Info", "Hello Leandro");
     }
 
