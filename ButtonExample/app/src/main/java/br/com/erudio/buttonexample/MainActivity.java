841957f9baf1e3c9fa755ac04fa89b08fbd06fda
leandrocgsi <leandrocgsi@gmail.com>
2017-10-29 19:16:27 -0200

    Implementing ButtonExample

--- ButtonExample/app/src/main/java/br/com/erudio/buttonexample/MainActivity.java
+++ ButtonExample/app/src/main/java/br/com/erudio/buttonexample/MainActivity.java
@@ -2,8 +2,14 @@ package br.com.erudio.buttonexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view){
+        Log.i("Info", "Hello Leandro");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
