57106d789d608eaf9c03c0e306ae392b7c1b8e43
leandrocgsi <leandrocgsi@gmail.com>
2017-10-29 19:47:10 -0200

    Implementing login

--- EditText/app/src/main/java/br/com/erudio/edittext/MainActivity.java
+++ EditText/app/src/main/java/br/com/erudio/edittext/MainActivity.java
@@ -2,8 +2,19 @@ package br.com.erudio.edittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void logIn(View view){
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+
+        Log.i("Username", username.getText().toString());
+        Log.i("Password", password.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,3 +22,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
