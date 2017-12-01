2fdc01c1771886fc27ff93550fabf4e038438544
leandrocgsi <leandrocgsi@gmail.com>
2017-12-01 16:52:03 -0200

    Working with toast

--- ToastDemo/app/src/main/java/br/com/erudio/toastdemo/MainActivity.java
+++ ToastDemo/app/src/main/java/br/com/erudio/toastdemo/MainActivity.java
@@ -3,6 +3,7 @@ package br.com.erudio.toastdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    public void sayHello(){
+    public void sayHello(View view){
 
         EditText etUsername = (EditText) findViewById(R.id.tfName);
         Log.i("Hello ", etUsername.getText().toString());
