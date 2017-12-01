650ed7228d2325f4ba696361d537cafaa7db9bcd
leandrocgsi <leandrocgsi@gmail.com>
2017-12-01 16:59:36 -0200

    Working with images

--- ImagesExample/app/src/main/java/br/com/erudio/imagesexample/MainActivity.java
+++ ImagesExample/app/src/main/java/br/com/erudio/imagesexample/MainActivity.java
@@ -2,8 +2,23 @@ package br.com.erudio.imagesexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public void sayHello(View view){
+
+        EditText etUsername = (EditText) findViewById(R.id.tfName);
+        Log.i("Hello ", etUsername.getText().toString());
+
+        String username = etUsername.getText().toString().toUpperCase();
+        Toast.makeText(getApplicationContext(), "HELLO " + username, Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
