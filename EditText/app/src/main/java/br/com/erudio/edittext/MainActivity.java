e4c493fb2f458509422f3ac01cbc4c89ca529189
leandrocgsi <leandrocgsi@gmail.com>
2017-12-01 15:45:35 -0200

    Working with toast

--- EditText/app/src/main/java/br/com/erudio/edittext/MainActivity.java
+++ EditText/app/src/main/java/br/com/erudio/edittext/MainActivity.java
@@ -5,17 +5,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     public void logIn(View view){
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText password = (EditText) findViewById(R.id.password);
+        EditText etUsername = (EditText) findViewById(R.id.username);
+        EditText etPassword = (EditText) findViewById(R.id.password);
 
-        Log.i(TAG + "_Username: ", username.getText().toString());
-        Log.i(TAG + "_Password: ", password.getText().toString());
+        Log.i(TAG + "_Username: ", etUsername.getText().toString());
+        Log.i(TAG + "_Password: ", etPassword.getText().toString());
+
+        String username = etUsername.getText().toString().toUpperCase();
+        Toast.makeText(getApplicationContext(), "HI " + username, Toast.LENGTH_LONG).show();
     }
 
     @Override
