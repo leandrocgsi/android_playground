05850dae03592e003e224857fa25ec3f5688baa1
leandrocgsi <leandrocgsi@gmail.com>
2017-12-01 16:47:32 -0200

    Working with toast

---
+++ ToastDemo/app/src/main/java/br/com/erudio/toastdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.erudio.toastdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public void sayHello(){
+
+        EditText etUsername = (EditText) findViewById(R.id.tfName);
+        Log.i("Hello ", etUsername.getText().toString());
+
+        String username = etUsername.getText().toString().toUpperCase();
+        Toast.makeText(getApplicationContext(), "HELLO " + username, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
