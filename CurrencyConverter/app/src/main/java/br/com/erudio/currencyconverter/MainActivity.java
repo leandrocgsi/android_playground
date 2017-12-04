b6eadba959092db29b866da75290a3baf16875e4
leandrocgsi <leandrocgsi@gmail.com>
2017-12-04 11:08:27 -0200

    Working with currency

--- CurrencyConverter/app/src/main/java/br/com/erudio/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/br/com/erudio/currencyconverter/MainActivity.java
@@ -2,16 +2,30 @@ package br.com.erudio.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     public void convert(View view){
+        EditText realField = (EditText) findViewById(R.id.realField);
+        String realStr = realField.getText().toString();
 
+        Double euroValue = 0.258906341D;
+        Double realValue = new Double(realStr);
+
+        Log.i(TAG, realStr);
+
+        double convertedValue = realValue * euroValue;
+        Toast.makeText(getApplicationContext(), "YOUR VALUE IN EURO IS " + convertedValue, Toast.LENGTH_LONG).show();
     }
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
