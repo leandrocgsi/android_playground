3c7a053a49a63bf8b90ec90456f3be0a57accbc3
leandrocgsi <leandrocgsi@gmail.com>
2017-12-05 14:21:22 -0200

    Building connect 3 game

--- Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
@@ -3,10 +3,18 @@ package br.com.erudio.connect3game;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void dropIn(View view){
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1000f).rotation(3600).setDuration(300);
+
 
     }
     @Override
