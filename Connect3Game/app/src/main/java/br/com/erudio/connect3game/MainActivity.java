6deceef358f9e33685242e5acd73443da4242cce
leandrocgsi <leandrocgsi@gmail.com>
2017-12-05 11:49:56 -0200

    Building connect 3 game

--- Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
@@ -2,9 +2,13 @@ package br.com.erudio.connect3game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void dropin(View view){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
