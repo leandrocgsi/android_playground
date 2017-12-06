89ebc74a852ca0aa4aeb2a9640498bbc061f0f1d
leandrocgsi <leandrocgsi@gmail.com>
2017-12-06 17:08:19 -0200

    Working with videos

--- VideoDemo/app/src/main/java/br/com/erudio/videodemo/MainActivity.java
+++ VideoDemo/app/src/main/java/br/com/erudio/videodemo/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.erudio.videodemo;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        videoView.start();
     }
 }
