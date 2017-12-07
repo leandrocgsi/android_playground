e39964efe456c2ecdfa9cc269c903181a73ab8cc
leandrocgsi <leandrocgsi@gmail.com>
2017-12-07 13:12:50 -0200

    Working with sound, video and animation

--- BasicPhrases/app/src/main/java/br/com/erudio/basicphrases/MainActivity.java
+++ BasicPhrases/app/src/main/java/br/com/erudio/basicphrases/MainActivity.java
@@ -2,8 +2,13 @@ package br.com.erudio.basicphrases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
