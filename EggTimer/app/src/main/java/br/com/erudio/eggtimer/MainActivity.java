e2f513b1afac7112c316cbc3193eccdfe0bbbe58
leandrocgsi <leandrocgsi@gmail.com>
2017-12-07 17:29:41 -0200

    Working with Timers and CountDown

--- EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
@@ -2,12 +2,41 @@ package br.com.erudio.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        TextView textView = (TextView) findViewById(R.id.timerTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                int minutes = (int) progress / 60;
+                int seconds = progress - minutes * 60;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
