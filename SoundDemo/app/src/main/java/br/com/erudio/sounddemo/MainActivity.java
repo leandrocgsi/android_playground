33015e4374207f85481044fed5ccbcbe402da882
leandrocgsi <leandrocgsi@gmail.com>
2017-12-07 10:41:51 -0200

    Working with sound

--- SoundDemo/app/src/main/java/br/com/erudio/sounddemo/MainActivity.java
+++ SoundDemo/app/src/main/java/br/com/erudio/sounddemo/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
@@ -42,6 +45,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.i("SeekBar value", Integer.toString(progress));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar seekBarPosition = (SeekBar)findViewById(R.id.seekBarPosition);
+        seekBarPosition.setMax(mediaPlayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBarPosition.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 100);
+
+        seekBarPosition.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("SeekBar position value", Integer.toString(progress));
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
             }
