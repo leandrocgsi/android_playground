7d297798d4e3378042125c0482a95385d26cac99
leandrocgsi <leandrocgsi@gmail.com>
2017-12-07 13:33:07 -0200

    Working with sound, video and animation

--- BasicPhrases/app/src/main/java/br/com/erudio/basicphrases/MainActivity.java
+++ BasicPhrases/app/src/main/java/br/com/erudio/basicphrases/MainActivity.java
@@ -1,13 +1,25 @@
 package br.com.erudio.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void buttonTapped(View view) {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
+    public void buttonTapped(View view) {
+        int id = view.getId();
+        String ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId =  getResources().getIdentifier(ourId, "raw", "br.com.erudio.basicphrases");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+        //Log.i(TAG + "Button tapped: ", Integer.toString(view.getId()));
+        Log.i(TAG + "Button tapped: ", ourId);
     }
 
     @Override
