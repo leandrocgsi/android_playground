fa9747ef22078776e79a6ce247adbfe29072986b
leandrocgsi <leandrocgsi@gmail.com>
2017-12-08 09:46:10 -0200

    Working with Timers and CountDown

--- EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.erudio.eggtimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +12,32 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+        if(seconds == 0) {
+            secondString = "00:00";
+        } else if (seconds < 10) {
+            secondString = "0" + Integer.toString(seconds);
+        }
+
+        //seconds == 0 ? "00" :  Integer.toString(seconds);
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = (TextView) findViewById(R.id.timerTextView);
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
@@ -26,12 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                int minutes = (int) progress / 60;
-                int seconds = progress - minutes * 60;
-
-                String secondString = seconds == 0 ? "00" :  Integer.toString(seconds);
-
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+                updateTimer(progress);
             }
 
             @Override
@@ -49,5 +64,19 @@ public class MainActivity extends AppCompatActivity {
     public void controlTimer (View view){
 
         Log.i(TAG + " Button pressed ! ", "Pressed");
+        new CountDownTimer(timerSeekBar.getProgress() * 1000,1000) {
+
+            public void onTick(long millisecondsUntilDone) {
+                Log.i(TAG + " Seconds left ! ", String.valueOf(millisecondsUntilDone / 1000));
+
+                updateTimer((int) millisecondsUntilDone / 1000);
+            }
+
+            public void onFinish() {
+                Log.i(TAG + " Finished ! ", "Timer done");
+
+                timerTextView.setText("00:00");
+            }
+        }.start();
     }
 }
