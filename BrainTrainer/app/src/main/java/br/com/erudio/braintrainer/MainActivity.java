360a50af0f49a34e08e7f9c8df32df3756f02ac9
leandrocgsi <leandrocgsi@gmail.com>
2017-12-08 13:52:07 -0200

    Building a simple game Brain Trainer App

--- BrainTrainer/app/src/main/java/br/com/erudio/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/br/com/erudio/braintrainer/MainActivity.java
@@ -2,12 +2,22 @@ package br.com.erudio.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+
+    public void start(View view){
+        startButton.setVisibility(View.INVISIBLE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
     }
 }
