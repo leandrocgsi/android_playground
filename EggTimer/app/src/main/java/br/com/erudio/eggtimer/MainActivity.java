455c8e0dc81540c8933d50702045203542369eee
leandrocgsi <leandrocgsi@gmail.com>
2017-12-08 10:31:34 -0200

    Working with Timers and CountDown

--- EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/br/com/erudio/eggtimer/MainActivity.java
@@ -1,12 +1,17 @@
 package br.com.erudio.eggtimer;
 
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import static android.os.SystemClock.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +19,31 @@ public class MainActivity extends AppCompatActivity {
 
     SeekBar timerSeekBar;
     TextView timerTextView;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("Go!");
+        counterIsActive = false;
+
+        /* sleep(2000);
+
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+
+        image.setImageResource(R.drawable.egg_smiling); */
+
+    }
 
     public void updateTimer(int secondsLeft) {
         int minutes = (int) secondsLeft / 60;
         int seconds = secondsLeft - minutes * 60;
 
-        String secondString = Integer.toString(seconds);
-        if (seconds < 10) {
-            secondString = "0" + Integer.toString(seconds);
-        }
-
-        //seconds == 0 ? "00" :  Integer.toString(seconds);
+        String secondString = (seconds < 10) ? "0" + Integer.toString(seconds): Integer.toString(seconds);
 
         timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
     }
@@ -36,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
         timerTextView = (TextView) findViewById(R.id.timerTextView);
+        goButton = (Button) findViewById(R.id.go);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
@@ -61,20 +81,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void controlTimer (View view){
 
-        Log.i(TAG + " Button pressed ! ", "Pressed");
-        new CountDownTimer(timerSeekBar.getProgress() * 1000,1000) {
+        if (!counterIsActive) {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("Stop");
 
-            public void onTick(long millisecondsUntilDone) {
-                Log.i(TAG + " Seconds left ! ", String.valueOf(millisecondsUntilDone / 1000));
+            Log.i(TAG + " Button pressed ! ", "Pressed");
 
-                updateTimer((int) millisecondsUntilDone / 1000);
-            }
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
 
-            public void onFinish() {
-                Log.i(TAG + " Finished ! ", "Timer done");
+                public void onTick(long millisecondsUntilDone) {
+                    Log.i(TAG + " Seconds left ! ", String.valueOf(millisecondsUntilDone / 1000));
 
-                timerTextView.setText("00:00");
-            }
-        }.start();
+                    updateTimer((int) millisecondsUntilDone / 1000);
+                }
+
+                public void onFinish() {
+                    Log.i(TAG + " Finished ! ", "Timer done");
+
+                    /* ImageView image = (ImageView) findViewById(R.id.imageView);
+
+                    image.setImageResource(R.drawable.broken_egg); */
+
+                    timerTextView.setText("00:00");
+                    resetTimer();
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mediaPlayer.start();
+                }
+            }.start();
+        } else {
+            resetTimer();
+        }
     }
 }
