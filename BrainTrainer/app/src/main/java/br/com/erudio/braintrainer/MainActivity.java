21bbc4cb8045617b5fd5a6d55685fa23bb486db6
leandrocgsi <leandrocgsi@gmail.com>
2017-12-20 15:18:06 -0200

    Implementing brain training

--- BrainTrainer/app/src/main/java/br/com/erudio/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/br/com/erudio/braintrainer/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.erudio.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     Button startButton;
     ArrayList<Integer> answers = new ArrayList<>();
@@ -20,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void chooseAnswer(View view){
-
+        Log.i(TAG + " Button pressed ! ", (String) view.getTag());
     }
 
     public void playAgain(View view){
