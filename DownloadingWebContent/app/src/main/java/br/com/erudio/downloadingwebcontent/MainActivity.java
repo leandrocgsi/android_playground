d47daac0d746f4c08569459ede586aa406271b0a
leandrocgsi <leandrocgsi@gmail.com>
2017-12-22 13:56:02 -0200

    Downloading web content

--- DownloadingWebContent/app/src/main/java/br/com/erudio/downloadingwebcontent/MainActivity.java
+++ DownloadingWebContent/app/src/main/java/br/com/erudio/downloadingwebcontent/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.erudio.downloadingwebcontent;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    public class DownloadTask extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            return null;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
