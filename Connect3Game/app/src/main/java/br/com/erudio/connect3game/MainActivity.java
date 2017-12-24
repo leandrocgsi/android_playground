6d1f30a6239200aabed9ff9e443b4865b5b3286d
leandrocgsi <leandrocgsi@gmail.com>
2017-12-24 21:49:53 -0200

    Adding log settings

--- Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/br/com/erudio/connect3game/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+	private static final String TAG = MainActivity.class.getSimpleName();
+
     int activePlayer = 0;
     boolean gameIsActive = true;
     int [] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
