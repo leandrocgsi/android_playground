1aae490064639050f1bfb099f23bbc9150a330d4
leandrocgsi <leandrocgsi@gmail.com>
2018-01-03 10:31:46 -0200

    Downloading web content

--- DownloadingWebContent/app/src/main/java/br/com/erudio/downloadingwebcontent/MainActivity.java
+++ DownloadingWebContent/app/src/main/java/br/com/erudio/downloadingwebcontent/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.net.HttpRetryException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -12,8 +17,21 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            return null;
+        protected String doInBackground(String... params) {
+
+            String result = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+            
+            try {
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Log.i(TAG + " Website URL:  ", params[0]);
+            return "Done";
         }
     }
     @Override
@@ -21,9 +39,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(TAG + " Button pressed ! ", "");
-
         DownloadTask task = new DownloadTask();
-        task.execute("https://www.ecowebhosting.co.uk/", "https://stackoverflow.com");
+        String result = null;
+        try {
+            result = task.execute("https://www.ecowebhosting.co.uk/", "https://stackoverflow.com").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.i(TAG + " Result! ", result);
     }
 }
