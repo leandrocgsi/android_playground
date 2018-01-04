8d3289218bc98132ab2d6c1870a00d3011579161
leandrocgsi <leandrocgsi@gmail.com>
2018-01-04 13:32:15 -0200

    Implementing guess celebrity game

--- GuessTheCelebrity/app/src/main/java/br/com/erudio/guessthecelebrity/MainActivity.java
+++ GuessTheCelebrity/app/src/main/java/br/com/erudio/guessthecelebrity/MainActivity.java
@@ -1,70 +1,121 @@
 package br.com.erudio.guessthecelebrity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+        import android.app.Activity;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.os.AsyncTask;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.Toast;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import java.util.ArrayList;
+        import java.util.Random;
+        import java.util.concurrent.ExecutionException;
+        import java.util.regex.Matcher;
+        import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends Activity {
 
-    public void celebrityChosen(View view) {
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
+    int locationOfCorrectAnswer = 0;
+    String[] answers = new String[4];
+
+    ImageView imageView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public void celebrityChosen (View view) {
+
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Toast.makeText(getApplicationContext(), "Wrong! It was " + celebNames.get(chosenCeleb), Toast.LENGTH_LONG).show();
+
+        }
+
+        createNewQuestion();
+
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
 
-        DownloadTask task = new DownloadTask();
-        String result = null;
 
-        try {
-            result = task.execute("http://posh24.com/celebrities").get();
+        @Override
+        protected Bitmap doInBackground(String... urls) {
 
-            Log.i(TAG + "Contents of URL", result);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            return null;
         }
     }
-    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
 
         @Override
         protected String doInBackground(String... urls) {
 
             String result = "";
-
             URL url;
             HttpURLConnection urlConnection = null;
 
-            Log.i(TAG + " Website URL:  ", urls[0]);
-
             try {
+
                 url = new URL(urls[0]);
 
-                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection = (HttpURLConnection)url.openConnection();
 
-                InputStream inputStream = urlConnection.getInputStream();
+                InputStream in = urlConnection.getInputStream();
 
-                InputStreamReader reader = new InputStreamReader(inputStream);
+                InputStreamReader reader = new InputStreamReader(in);
 
                 int data = reader.read();
 
                 while (data != -1) {
+
                     char current = (char) data;
 
                     result += current;
@@ -73,12 +124,148 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 return result;
-            } catch (Exception e) {
+
+            }
+            catch (Exception e) {
+
                 e.printStackTrace();
-                return "Failed!";
+
             }
 
+            return null;
         }
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+
+            result = task.execute("http://www.posh24.com/celebrities").get();
+
+            String[] splitResult = result.split("<div class=\"sidebarContainer\">");
+
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+
+                celebURLs.add(m.group(1));
+
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+
+                celebNames.add(m.group(1));
+
+            }
+
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        } catch (ExecutionException e) {
+
+            e.printStackTrace();
+
+        }
+
+        createNewQuestion();
+
+    }
+
+    public void createNewQuestion() {
+
+        Random random = new Random();
+        chosenCeleb = random.nextInt(celebURLs.size());
+
+        ImageDownloader imageTask = new ImageDownloader();
+
+        Bitmap celebImage;
+
+        try {
+
+            celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
+            locationOfCorrectAnswer = random.nextInt(4);
+
+            int incorrectAnswerLocation;
+
+            for (int i=0; i<4; i++) {
+
+                if (i == locationOfCorrectAnswer) {
+
+                    answers[i] = celebNames.get(chosenCeleb);
+
+                } else {
+
+                    incorrectAnswerLocation = random.nextInt(celebURLs.size());
+
+                    while (incorrectAnswerLocation == chosenCeleb) {
+
+                        incorrectAnswerLocation = random.nextInt(celebURLs.size());
+
+                    }
+
+                    answers[i] = celebNames.get(incorrectAnswerLocation);
+
+
+                }
+
+
+            }
+
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
