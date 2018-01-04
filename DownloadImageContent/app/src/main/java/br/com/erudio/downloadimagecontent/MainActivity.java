50636cab86d51a740aeb6963f40e49703edae19f
leandrocgsi <leandrocgsi@gmail.com>
2018-01-04 09:23:50 -0200

    Downloading image content

--- DownloadImageContent/app/src/main/java/br/com/erudio/downloadimagecontent/MainActivity.java
+++ DownloadImageContent/app/src/main/java/br/com/erudio/downloadimagecontent/MainActivity.java
@@ -2,12 +2,18 @@ package br.com.erudio.downloadimagecontent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void downloadImage(View view) {
     }
 }
