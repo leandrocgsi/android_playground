553454e3ced86e1f613c1081a680fbfaf92159f7
leandrocgsi <leandrocgsi@gmail.com>
2018-01-04 15:31:14 -0200

    Consuming JSON

--- JSONDemo/app/src/main/java/br/com/erudio/jsondemo/MainActivity.java
+++ JSONDemo/app/src/main/java/br/com/erudio/jsondemo/MainActivity.java
@@ -1,9 +1,72 @@
 package br.com.erudio.jsondemo;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            Log.i(TAG + " Website URL:  ", urls[0]);
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
