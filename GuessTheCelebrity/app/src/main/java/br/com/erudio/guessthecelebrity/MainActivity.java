8e2bcf2a7009c1ea1e8c41dd1fb658033369aecf
leandrocgsi <leandrocgsi@gmail.com>
2018-01-04 11:29:35 -0200

    Implementing guess celebrity game

--- GuessTheCelebrity/app/src/main/java/br/com/erudio/guessthecelebrity/MainActivity.java
+++ GuessTheCelebrity/app/src/main/java/br/com/erudio/guessthecelebrity/MainActivity.java
@@ -1,17 +1,84 @@
 package br.com.erudio.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public void celebrityChosen(View view) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://posh24.com/celebrities").get();
+
+            Log.i(TAG + "Contents of URL", result);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            Log.i(TAG + " Website URL:  ", urls[0]);
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed!";
+            }
+
+        }
     }
 
-    public void celebrityChosen(View view) {
-    }
 }
