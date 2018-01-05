6530babd1c099286f5fca0481b5a193fe456a01f
leandrocgsi <leandrocgsi@gmail.com>
2018-01-05 09:58:44 -0200

    Implementing 'Waths The Weather' app

--- WathsTheWeather/app/src/main/java/br/com/erudio/wathstheweather/MainActivity.java
+++ WathsTheWeather/app/src/main/java/br/com/erudio/wathstheweather/MainActivity.java
@@ -2,8 +2,13 @@ package br.com.erudio.wathstheweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void findWeather(View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
