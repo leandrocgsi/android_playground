8e8aabc11cfc6205452683bf5a2d1cc7a9813ee4
leandrocgsi <leandrocgsi@gmail.com>
2018-01-14 21:54:10 -0200

    Adding log settings

--- SimpleMenu/app/src/main/java/br/com/erudio/simplemenu/MainActivity.java
+++ SimpleMenu/app/src/main/java/br/com/erudio/simplemenu/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
